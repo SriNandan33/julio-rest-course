816b60e802c8044270bf15542bb6e68d2ca62862
Srinivasa Rao M <srinivasarao.m@everest.engineering>
2023-10-17 17:59:15 +0530

    add parameter validation

--- Entities/Game.cs
+++ Entities/Game.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameStore.Entities;
 
 public class Game
 {
     public int Id { get; set; }
+    [Required]
+    [MaxLength(100)]
     public required string Name { get; set; }
+    [Required]
     public required string Genre { get; set; }
+    [Range(1, 100)]
     public decimal Price { get; set; }
     public DateTime ReleaseDate { get; set; }
+    [Url]
+    [MaxLength(100)]
     public required string ImageUri { get; set; }
 }
