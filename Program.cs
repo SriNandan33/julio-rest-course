edec233ad81d98a7c1cd4d0edfbf8e4e771095c7
Srinivasa Rao M <srinivasarao.m@everest.engineering>
2023-10-17 13:46:51 +0530

    add Game entity

--- Program.cs
+++ Program.cs
@@ -1,6 +1,46 @@
+
+using GameStore.Entities;
+
+List<Game> games = new()
+{
+    new Game(){
+        Id = 1,
+        Name = "PubG",
+        Price = 20.5M,
+        Genre = "Shooting",
+        ImageUri = "https://placehold.co/100",
+        ReleaseDate = new DateTime(2018, 1, 1)
+    },
+    new Game(){
+        Id = 2,
+        Name = "FreeFire",
+        Price = 20.5M,
+        Genre = "Shooting",
+        ImageUri = "https://placehold.co/100",
+        ReleaseDate = new DateTime(2019, 1, 1)
+    },
+    new Game(){
+        Id = 3,
+        Name = "NFS - Need for Speed",
+        Price = 50.5M,
+        Genre = "Racing",
+        ImageUri = "https://placehold.co/100",
+        ReleaseDate = new DateTime(2015, 1, 1)
+    },
+    new Game(){
+        Id = 4,
+        Name = "Chess 2.0",
+        Price = 5.5M,
+        Genre = "Board",
+        ImageUri = "https://placehold.co/100",
+        ReleaseDate = new DateTime(2018, 1, 1)
+    },
+};
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/games", () => games);
 
 app.Run();
