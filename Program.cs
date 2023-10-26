059bbff5be803acbe892dd9ecb8c6584feefb84b
Srinivasa Rao M <srinivasarao.m@everest.engineering>
2023-10-26 14:45:46 +0530

    sql server docker compose and read connection string froms secret

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ using GameStore.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IGamesRepository, InMemGamesRepository>();
 
+var connString = builder.Configuration.GetConnectionString("GameStoreDatabase");
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
